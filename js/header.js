e44bdc03e40f64e8bbe9f30495e4a60bcc32d2c0
Arturo <Arturo@Arturo-PC>
2014-11-18 13:49:00 -0500

    usando BD server

--- js/header.js
+++ js/header.js
@@ -3,7 +3,39 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function test(place){
+function locator(location){
+    var locale = location.getAttribute("name");
+    var address = ""
+    //From main menu
+    if (locale=="Administración"){
+        address = "menu_admin.php"
+    }else if(locale=="Logística"){
+         address = "logistic.php"
+    }
+    else if(locale=="Programación"){
+        address = "menu_devel.php"
+    }
+    //From logistic menu
+    else if(locale=="Fichas"){
+        //go to reports
+    }
+    else if(locale=="Itinerario"){
+        //go to reports
+    }
+    else if(locale=="Calendarios"){
+        //go to reports
+    }
+    else if(locale=="Evaluaciones"){
+        //go to reports
+    }
+    else if(locale=="Reportes"){
+        //go to reports
+    }
+    else if(locale=="Configuraciones"){
+        address = "settings.php"
+    }
+    //From other menus add else if(locale=="name attribute"){}
     
+    window.location.href = address + "?position=" + locale;
 }
 
