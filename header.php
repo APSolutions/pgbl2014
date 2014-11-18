e2df18e20176967015f0a7121ca15ccd8f244f4d
Cesar_Patino <Cesar_Patino@WN7X64-977YD12.pty.amer.dell.com>
2014-11-18 00:19:29 -0500

    header update 1

--- header.php
+++ header.php
@@ -8,18 +8,20 @@ $lastname = $user->get_lastname();
     echo <<<_END
     <header class="clearfix">
                     <div class="header-user">
-                        <span class="userInfo">
-                           Hi $name, $lastname |
-                        </span>
-                        <span class="logout">
-                            <a href="logout.php"> logout </a>
-                        </span>
+                        <p class="userInfo">
+                           Hi $name, $lastname
+                        </p>
+                        <a href="#"> 
+                            <span class="header-user-icon flaticon-bell46" id="notifications" data-content="notifications"></span>
+                        </a>
+                        <a href="logout.php"> 
+                            <span class="header-user-icon flaticon-logout13" id="logout" data-content="logout"></span>  
+                        </a>                        
                     </div>
                     <div class="header-logo">
                         <span> 
                             <img class="logo" src="img/logo.png"/> 
-                            <p class="slogan"> Students Empowering Communities
-                            <span class="bp-icon bp-icon-about" data-content="help button"></span>                                        
+                            <p class="slogan"> Students Empowering Communities                                                                
                             </p> 
                         </span>
                     </div>
