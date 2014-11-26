cdb8508a31c1da71d855f27843c44b3dfe9b2b3f
Cesar_Patino <Cesar_Patino@WN7X64-977YD12.pty.amer.dell.com>
2014-11-26 06:31:27 -0500

    header medio arreglado

--- header.php
+++ header.php
@@ -12,19 +12,19 @@ $position = $_SESSION["position"];
 
 
 echo <<<END
-    <header class="container">
+    <header class="clearfix">
         <div class="header-logo">
             <img class="img-logo" src="img/logo.png"/>
             <p class="slogan">Students Empowering Communities</p> 
         </div>
         <div class="header-user">
             <p class="user-details">Hi $name, $lastname</p>
-            <a href="#" class="tooltip" data-content="notifications">
-                <span class="header-user-icon flaticon-bell46" id="notifications">2</span>
-            </a>
-            <a href="logout.php" class="tooltip" data-content="logout"> 
-                <span class="header-user-icon flaticon-logout13" id="logout">1</span>  
-            </a>                        
+            <a href="#" class="tooltip">
+                    <span class="header-icon flaticon-bell" data-content="notifications"></span>
+                </a>
+                <a href="logout.php" class="tooltip"> 
+                    <span class="header-icon flaticon-logout" data-content="logout"></span>  
+                </a>                          
         </div> 
         <div class="header-title">
             <h1 class="position">$position</h1>
