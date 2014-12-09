ef65888069b715414da00fae9f8cb364421b4116
Cesar_Patino <Cesar_Patino@WN7X64-977YD12.pty.amer.dell.com>
2014-12-09 08:17:17 -0500

    clase y loader de la ficha

---
+++ src/ficha/ficha.php
@@ -0,0 +1,207 @@
+<?php
+
+/**
+ * Description of ficha
+ *
+ * @author AP Solutions
+ */
+class Ficha {
+    //atributes
+    private $id, $iDate, $fDate, $dDate, $aDate, $tStudents, $cityTour;
+    private $compoundID, $communityID;
+    private $flights = array(array(
+        "id" => "",
+        "type" => "",
+        "arrivalTime" => "",
+        "tStudents" => ""
+    ));
+    private $vehicles = array();
+    private $staff = array(array(
+        "id" => "",
+        "role" => ""
+    ));
+    //methods
+    public function _construct($param_id){
+        $this->id = $param_id;
+        $this->obtainVariables();
+        $this->obtainFlights();
+        $this->obtainVehicles();
+        $this->obtainStaff();
+    }
+    
+    private function obtainVariables(){
+       try {
+           require_once '../login/connect.php';
+           $query = "";
+           $result = $conn->query($query);
+           $this->iDate = "";
+           $this->fDate = "";
+           $this->dDate = "";
+           $this->aDate = "";
+           $this->tStudents = "";
+           $this->cityTour = "";
+           $this->compoundID = "";
+           $this->communityID = "";
+           return 'ok';
+       } catch (Exception $ex) {
+           return 'error';
+       } 
+    }
+    
+    private function obtainFlights(){
+        try {
+            require_once '../login/connect.php';
+            $query = "";
+            $result = $conn->query($query);
+            //logica para sacar los vuelos
+            return 'ok';
+        } catch (Exception $ex) {
+            return 'error';
+        }      
+    }
+    
+    private function obtainVehicles(){
+        try {
+            require_once '../login/connect.php';
+            $query = "";
+            $result = $conn->query($query);
+            //logica para sacar los vehiculos
+            return 'ok';
+        } catch (Exception $ex) {
+            return 'error';
+        }
+    }
+    
+    private function obtainStaff(){
+        try {
+            require_once '../login/connect.php';
+            $query = "";
+            $result = $conn->query($query);
+            //logica para sacar el staff
+            return 'ok';
+        } catch (Exception $ex) {
+            return 'error';
+        }
+    }
+    
+    public function getFichaData(){
+        return $result= array(
+            "id" => $this->id,
+            "iDate" => $this->iDate,
+            "fDate" => $this->fDate,
+            "dDate" => $this->dDate,
+            "aDate" => $this->aDate,
+            "tStudents" => $this->tStudents,
+            "cityTour" => $this->cityTour,
+            "compoundID" => $this->compoundID,
+            "communityID" => $this->communityID
+        );
+    }
+    
+    public function getFlights() {
+        return $this->flights;
+    }
+    
+    public function getVehicles() {
+        return $this->vehicles;
+    }
+    
+    public function getSatff(){
+        return $this->staff;
+    }
+
+    public function setiDate($param_iDate){
+        try{
+            $this->iDate = $param_iDate;
+            //query para insertar el campo
+        } catch (Exception $ex) {
+
+        }
+    }
+    
+    public function setfDate($param_fDate){
+        try{
+            $this->fDate = $param_fDate;
+            //query para insertar el campo
+        } catch (Exception $ex) {
+
+        }
+    }
+    public function setdDate($param_dDate){
+        try{
+            $this->dDate = $param_dDate;
+            //query para insertar el campo
+        } catch (Exception $ex) {
+
+        }
+    }
+    public function setaDate($param_aDate){
+        try{
+            $this->aDate = $param_aDate;
+            //query para insertar el campo
+        } catch (Exception $ex) {
+
+        }
+    }
+    public function setTStudents($param_tStudents){
+        try{
+            $this->tStudents = $param_tStudents;
+            //query para insertar el campo
+        } catch (Exception $ex) {
+
+        }
+    }
+    public function setCityTour($param_cityTour){
+        try{
+            $this->cityTour = $param_cityTour;
+            //query para insertar el campo
+        } catch (Exception $ex) {
+
+        }
+    }
+    public function setCompound($param_compound){
+        try{
+            $this->compoundID = $param_compound;
+            //query para insertar el campo
+        } catch (Exception $ex) {
+
+        }
+    }
+    public function setCommunity($param_community){
+        try{
+            $this->communityID = $param_community;
+            //query para insertar el campo
+        } catch (Exception $ex) {
+
+        }
+    }
+    public function setFight($param_id, $param_type, $param_arrivalTime, $param_tStudents){
+        try{
+            $this->flights[i]["id"] = $param_id;
+            $this->flights[i]["type"] = $param_type;
+            $this->flights[i]["arrivalTime"] = $param_arrivalTime;
+            $this->flights[i]["tStudents"] = $param_tStudents;
+            //query para insertar el campo
+        } catch (Exception $ex) {
+
+        }
+    }
+    public function setVehicle($param_vehicle){
+        try{
+            $this->vehicles[$i] = $param_vehicle;
+            //query para insertar el campo
+        } catch (Exception $ex) {
+
+        }
+    }
+    public function setStaff($param_staff, $param_role){
+        try{
+            $this->staff[i]["id"] = $param_staff;
+            $this->staff[i]["role"] = $param_role;
+            //query para insertar el campo
+        } catch (Exception $ex) {
+
+        }
+    }
+    
+}
---
+++ src/ficha/prepareFicha.php
@@ -0,0 +1,18 @@
+<?php
+require 'ficha.php';
+
+session_name('Global');
+session_id('pgbl');
+session_start();    
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if (isset($_GET["fichaID"])) {
+        $_SESSION["ficha"] = new Ficha($_GET["fichaID"]);
+        $address = "../../ficha.php";
+    }
+}
+
+header("Location:$address");
+    
+    
+
