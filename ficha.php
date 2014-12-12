ba77dcf853772f31f7aca81874cb4f605defb09f
Cesar_Patino <Cesar_Patino@WN7X64-977YD12.pty.amer.dell.com>
2014-12-12 07:58:22 -0500

    faltantes de la ficha bd y clase

--- ficha.php
+++ ficha.php
@@ -12,11 +12,11 @@ $ficha = $_SESSION["ficha"];
     <head>
         <!-- Page description-->
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Global Brigades Panama: Fichas" />
         <meta name="keywords" content="global brigades, panama, fichas, programa" />
-        <meta name="author" content="APSolution" />        
+        <meta name="author" content="APSolution" />
         <title>Global Brigades Panama | Fichas</title>
         <link rel="shortcut icon" href="img/favicon.ico"/>
         <!-- CSS scripts-->
@@ -33,14 +33,21 @@ $ficha = $_SESSION["ficha"];
                 <?php
                 $fichaGenerals = $ficha->getFichaData();
                 ?>
-                <span id="fichaID"><?php echo $fichaGenerals["id"]?></span>
-                <span><?php echo $fichaGenerals["program"]?></span>
-                <label> Comunidad </label>
-                <span><?php echo $fichaGenerals["community"]?></span>                
-                <label> Campamento </label>
-                <span><?php echo $fichaGenerals["compound"]?></span>                
+                <h2 class="ficha-tittle"><?php echo $fichaGenerals["id"]?>
+                    <span class="ficha-program"><?php echo $fichaGenerals["program"]?></span>
+                </h2>
+                <h2 class="ficha-places"> Comunidad </h2>
+                <a href="" class="ficha-places-data">
+                    <span><?php echo $fichaGenerals["community"]?></span>
+                </a>
+                <h2 class="ficha-places"> Campamento </h2>
+                <a href="" class="ficha-places-data">
+                    <span><?php echo $fichaGenerals["compound"]?></span>
+                </a>
+                <!--
+                http://tympanus.net/Blueprints/TooltipMenu/#
+                -->
             </div>
         </div>
-        
     </body>
 </html>
