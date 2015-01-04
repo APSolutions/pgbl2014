7246dd103b74c370509b3dd791877f435c565dc7
Arturo <Arturo@Arturo-PC>
2015-01-03 23:47:05 -0500

    calendario casi listo

--- brigades_calendar.php
+++ brigades_calendar.php
@@ -6,21 +6,6 @@
     $_SESSION["position"] = "Calendario de Brigadas";
     $_SESSION["action"] = "";
     $_SESSION["program"] = "";
-    if(!isset($_REQUEST['day'])) $_REQUEST['day'] = date('d');
-    if (!isset($_REQUEST["month"])) $_REQUEST["month"] = date("n");
-    if (!isset($_REQUEST["year"])) $_REQUEST["year"] = date("Y");
-
-    $monthNames = Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
-                        "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
-    $cDay = $_REQUEST['day'];
-    $cMonth = $_REQUEST["month"];
-    $cYear = $_REQUEST["year"]; 
-
-    $next_month = getDate(mktime(0,0,0,$cMonth+1,$cDay,$cYear));
-    $prev_month = getDate(mktime(0,0,0,$cMonth-1,$cDay,$cYear));
-
-    $next_year = getDate(mktime(0,0,0,$cMonth,$cDay,$cYear+1));
-    $prev_year =getDate(mktime(0,0,0,$cMonth,$cDay,$cYear-1));
 ?>
     
 <!DOCTYPE html>
@@ -35,60 +20,76 @@
         <meta name="author" content="APSolution" />
         <link rel="shortcut icon" href="img/favicon.ico"/>
         <link rel="stylesheet" type="text/css" href="css/header.css" />
-        <link rel="stylesheet" type="text/css" href="css/calendar.css" />
-        <script src="js/logistic_menu/modernizr.custom.js"></script>
+        <link href='css/calendars/fullcalendar.css' rel='stylesheet' />
+        <link href='css/calendars/fullcalendar.print.css' rel='stylesheet' media='print' />
+        <script src="js/logistic_menu/modernizr.custom.js"></script>     
+        <script src='js/calendars/lib/moment.min.js'></script>
+        <script src='js/calendars/lib/jquery.min.js'></script>
+        <script src='js/calendars/fullcalendar.min.js'></script>
+        <script>
+
+                $(document).ready(function() {
+
+                        $('#calendar').fullCalendar({
+                                eventLimit: true, // allow "more" link when too many events
+                                events: {
+                                        url: 'src/calendars/get-events.php',
+                                        error: function() {
+                                                $('#script-warning').show();
+                                        }
+                                },
+                                loading: function(bool) {
+                                        $('#loading').toggle(bool);
+                                }
+                        });
+
+                });
+
+        </script>
+        <style>
+            body {
+                margin: 0;
+                padding: 0;
+                font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
+                font-size: 14px;
+            }
+
+            #script-warning {
+                display: none;
+                background: #eee;
+                border-bottom: 1px solid #ddd;
+                padding: 0 10px;
+                line-height: 40px;
+                text-align: center;
+                font-weight: bold;
+                font-size: 12px;
+                color: red;
+            }
+
+            #loading {
+                display: none;
+                position: absolute;
+                top: 10px;
+                right: 10px;
+            }
+
+            #calendar {
+                max-width: 900px;
+                margin: 40px auto;
+                padding: 0 10px;
+            }
+        </style>
     </head>
     <body>
         <div class="container">
             <?php
             require 'header.php';
-            ?>	
-        </div>  
-        <table class="calendar_header">
-            <tr>
-                <td>
-                    <a class="link" href="<?php echo $_SERVER['PHP_SELF']."?day=". $prev_year['mday'] ."&year=". $prev_year['year'] ."&month=". $prev_year['mon'] ?>" ><</a>
-                    <strong><?php echo $cYear; ?></strong>
-                    <a class="link" href="<?php echo $_SERVER['PHP_SELF']."?day=". $next_year['mday'] ."&year=". $next_year['year'] ."&month=". $next_year['mon'] ?>" >> </a>
-                </td>
-            </tr>
-            <tr>                           
-                <td>
-                    <a class="link" href="<?php echo $_SERVER['PHP_SELF']."?day=". $prev_month['mday'] ."&year=". $prev_month['year'] ."&month=". $prev_month['mon'] ?>">< </a>
-                    <strong><?php echo $monthNames[$cMonth-1]; ?></strong>
-                    <a class="link" href="<?php echo $_SERVER['PHP_SELF']."?day=". $next_month['mday'] ."&year=". $next_month['year'] ."&month=". $next_month['mon']; ?>">> </a>
-                </td>
-            </tr>
-            <tr>
-                <td><a class="link" href="<?php echo $_SERVER["PHP_SELF"];?>">Día Actual</a></td>
-            </tr>
-        </table>
-        <table class="gridtable" >   
-            <tr class="tr_days" >
-                <td><strong>Domingo</strong></td>
-                <td><strong>Lunes</strong></td>
-                <td><strong>Martes</strong></td>
-                <td><strong>Miercoles</strong></td>
-                <td><strong>Jueves</strong></td>
-                <td><strong>Viernes</strong></td>
-                <td><strong>Sabado</strong></td>
-            </tr>
-            <?php 
-
-            $timestamp = mktime(0,0,0,$cMonth,1,$cYear);
-            $maxday = date("t",$timestamp);                 
-            $thismonth = getdate ($timestamp);             
-            $startday = $thismonth['wday'];                 
-            for ($i=0; $i<($maxday+$startday); $i++) {
-                if(($i % 7) == 0 ) echo "<tr>";
-                if($i < $startday) echo "<td></td>";
-                elseif(($i - $startday + 1) == $cDay){
-                    echo "<td bgcolor='red'> ". ($i - $startday + 1) ."</td>";}
-                else{
-                    echo "<td>". ($i - $startday + 1) ."</td>";}
-                if(($i % 7) == 6 ) echo "</tr>";
-            }
             ?>
-        </table>
+        <div id='script-warning'>
+            <code>src/calendars/get-events.php</code> must be running.
+	</div>
+	<div id='loading'>loading...</div>
+	<div id='calendar'></div>
+        </div>
     </body>
 </html>
